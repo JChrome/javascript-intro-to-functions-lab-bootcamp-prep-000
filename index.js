fcb640846a2162ea0bc0f41303e0e24c6553b6a1
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-02-09 21:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,16 +18,10 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return "I can't hear you!"
   }
-  else if(string.toUpperCase() === string){
+  else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
   else {
     return "I love you, too."
-  }
-}
-
-function sayHiToGrandma(string){
-  if(string.toLowerCase() === string){
-    return "I can't hear you!"
   }
 }
